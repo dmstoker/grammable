46722cde082a8121310b56916e76d2821ce10609
Dylan Stoker <dammitdylan88@gmail.com>
2020-01-07 05:35:00 +0000

    added prams requring only the user who created a post have control of updating/editing/destroying

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,19 +1,17 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+    def update
+      @gram = Gram.find_by_id(params[:id])
+      return render_not_found if @gram.blank?
+      return render_not_found(:forbidden) if @gram.user != current_user
 
-  def update
-    @gram = Gram.find_by_id(params[:id])
-    return render_not_found if @gram.blank?
-
-    @gram.update_attributes(gram_params)
-
-    if @gram.valid?
-      redirect_to root_path
-    else
-      return render :edit, status: :unprocessable_entity
+      @gram.update_attributes(gram_params)
+      if @gram.valid?
+        redirect_to root_path
+      else
+        return render :edit, status: :unprocessable_entity
+      end
     end
-  end
-
 
   def new
     @gram = Gram.new
@@ -28,9 +26,10 @@ class GramsController < ApplicationController
   end
 
   def edit
-    @gram = Gram.find_by_id(params[:id])
-    return render_not_found if @gram.blank?
-  end
+      @gram = Gram.find_by_id(params[:id])
+      return render_not_found if @gram.blank?
+      return render_not_found(:forbidden) if @gram.user != current_user
+    end
 
   def create
     @gram = current_user.grams.create(gram_params)
@@ -45,6 +44,7 @@ class GramsController < ApplicationController
   def destroy
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
     @gram.destroy
     redirect_to root_path
   end
@@ -55,7 +55,7 @@ class GramsController < ApplicationController
     params.require(:gram).permit(:message)
   end
 
-  def render_not_found
-    render plain: 'Not Found :(', status: :not_found
+  def render_not_found(status=:not_found)
+   render plain: "#{status.to_s.titleize} :(", status: status
   end
 end
